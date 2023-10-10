a7c128e21210e051391c034480df8a5ff102b1a6
Seonu1015 <ulxovoxlu1015@gmail.com>
2023-10-10 17:06:03 +0900

    1010 오후 수업

---
+++ study_1010/class03/routes/users.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+var db = require("../db");
+
+/* GET users listing. */
+router.get('/list.json', function(req, res, next) {
+  var sql = 'select * from users';
+  db.get().query(sql, function(err, rows){
+    res.send(rows);
+  });
+});
+
+//사용자 목록
+router.get('/', function(req, res){
+    res.render('index', {title:'사용자 목록', pageName:'users/list.ejs'});
+});
+
+module.exports = router;
