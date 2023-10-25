098771738821ed0099647376b4bf78b9c6dc2e65
Seonu1015 <ulxovoxlu1015@gmail.com>
2023-10-25 16:32:01 +0900

    1025 오후 수업

---
+++ w2_react/study_1025/class02/components/ex03/BookModal.jsx
@@ -0,0 +1,56 @@
+import { useState } from 'react';
+import { Button, Modal, Row, Col } from 'react-bootstrap'
+import Book from './Book';
+
+const BookModal = ({ book }) => {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    return (
+        <>
+            <Button variant="primary" onClick={handleShow}>
+                상세보기
+            </Button>
+
+            <Modal
+                size="lg"
+                show={show}
+                onHide={handleClose}
+                backdrop="static"
+                keyboard={false}
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title>상세정보</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Row>
+                        <Col md={3} className='text-center'>
+                            <img src={book.thumbnail || "http://via.placeholder.com/170x250"} width="100%" />
+                        </Col>
+                        <Col>
+                            <h3>{book.title}({book.authors},{book.publisher})</h3>
+                            <hr />
+                            <p>가격 : {book.price}</p>
+                            <p>ISBN : {book.isbn}</p>
+                            <p>출판일 : {book.datetime}</p>
+                        </Col>
+                    </Row>
+                    <hr />
+                    <div>
+                        {book.contents || <p>내용없음</p>}
+                    </div>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                    <Button variant="primary">Understood</Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    )
+}
+
+export default BookModal
---
+++ w2_react/study_1025/class02/components/ex03/LocalSearch.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LocalSearch = () => {
+  return (
+    <div className='m-5'>
+        <h1 className='text-center'>지역검색</h1>
+    </div>
+  )
+}
+
+export default LocalSearch
