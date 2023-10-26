6d0308c83b93bf7fba5a6574da952e84bce52912
Seonu1015 <ulxovoxlu1015@gmail.com>
2023-10-26 12:48:41 +0900

    1026 오전 수업

---
+++ w2_react/study_1026/class01/components/ex03/BlogSearch.jsx
@@ -0,0 +1,104 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { Table, Button, InputGroup, Form, Row, Col } from 'react-bootstrap'
+import { useLocation, useNavigate } from 'react-router-dom'
+
+const BlogSearch = () => {
+    const [loading, setLoading] = useState(false);
+    const [blogs, setBlogs] = useState([]);
+    const [total, setTotal] = useState(0);
+    const [end, setEnd] = useState(false);
+
+    const navigate = useNavigate();
+    const location = useLocation();
+    // console.log(location);
+    const search = new URLSearchParams(location.search);
+    // console.log(search);
+    const page = parseInt(search.get("page"));
+    const [query, setQuery] = useState(search.get("query"));
+    // console.log(page, query);
+
+    const getBlogs = async () => {
+        const url = `https://dapi.kakao.com/v2/search/blog?size=5&page=${page}&query=${query}`;
+        const config = {
+            headers: {
+                "Authorization": "KakaoAK d98342bfb3d10bd8a8d18f10982fe1c8"
+            }
+        }
+        setLoading(true);
+        const res = await axios(url, config);
+        console.log(res.data);
+        let data = res.data.documents;
+        data = data.map(blog => blog && {...blog, show:false});
+        setBlogs(data);
+        setEnd(res.data.meta.is_end);
+        setTotal(res.data.meta.pageable_count);
+        setLoading(false);
+    }
+
+    useEffect(() => {
+        getBlogs();
+    }, [location])
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        navigate(`/blog?page=1&query=${query}`);
+    }
+
+    const onClick = (idx) => {
+        let data = blogs.map((blog, index) => index === idx ? {...blog, show:!blog.show} : blog);
+        setBlogs(data);
+    }
+
+    return (
+        <>
+            <div className='m-5'>
+                <h1 className='text-center mb-5'>블로그검색</h1>
+                {loading ?
+                    <div>로딩중....</div>
+                    :
+                    <>
+                        <Row className='mb-3 justify-content-end'>
+                            <Col md={4}>
+                                <form onSubmit={onSubmit}>
+                                    <InputGroup>
+                                        <Form.Control value={query} onChange={(e) => setQuery(e.target.value)} />
+                                        <Button type='submit'>검색</Button>
+                                    </InputGroup>
+                                </form>
+                            </Col>
+                        </Row>
+                        <Table>
+                            <thead>
+                                <tr>
+                                    <th>BLOG</th>
+                                    <th>TITLE</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {blogs.map((blog, index) =>
+                                    <tr key={blog.url}>
+                                        <td width="30%"><a href={blog.url}>{blog.blogname}</a></td>
+                                        <td>
+                                            <div onClick={() => onClick(index)} dangerouslySetInnerHTML={{ __html: blog.title }} style={{cursor:"pointer", color:"brown"}}/>
+                                            {blog.show &&
+                                                <div dangerouslySetInnerHTML={{ __html: blog.contents }} />
+                                            }
+                                        </td>
+                                    </tr>
+                                )}
+                            </tbody>
+                        </Table>
+                        <div className='text-center'>
+                            <Button onClick={() => navigate(`/blog?page=${page - 1}&query=${query}`)} disabled={page === 1}>이전</Button>
+                            <span> {page} / {Math.ceil(total / 5)} </span>
+                            <Button onClick={() => navigate(`/blog?page=${page + 1}&query=${query}`)} disabled={end}>다음</Button>
+                        </div>
+                    </>
+                }
+            </div>
+        </>
+    )
+}
+
+export default BlogSearch
