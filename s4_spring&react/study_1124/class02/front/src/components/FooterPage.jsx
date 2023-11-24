d4d5a823695b7893cf16ff4ad04c508af0936272
Seonu1015 <ulxovoxlu1015@gmail.com>
2023-11-24 18:23:41 +0900

    1124 오후 수업

---
+++ s4_spring&react/study_1124/class02/front/src/components/FooterPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const FooterPage = () => {
+  return (
+    <div className='my-5'>
+        <hr/>
+        <h6 className='my-5 text-center'>Copyright 2023. SEON All rights reserved.</h6>
+    </div>
+  )
+}
+
+export default FooterPage
