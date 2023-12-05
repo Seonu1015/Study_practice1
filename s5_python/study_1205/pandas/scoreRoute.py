6729bcd6666be07bd28afd5f9950240e1a4fbd93
Seonu1015 <ulxovoxlu1015@gmail.com>
2023-12-05 18:26:48 +0900

    1205 오후 수업

---
+++ s5_python/study_1205/pandas/scoreRoute.py
@@ -0,0 +1,36 @@
+from flask import render_template, Blueprint, send_file
+from io import BytesIO
+import pandas as pd
+import matplotlib.pyplot as plt
+
+plt.rcParams['font.family'] = 'Malgun Gothic'
+plt.rcParams['font.size'] = 10
+plt.rcParams['axes.unicode_minus'] = False
+
+score = Blueprint('score', __name__)
+
+@score.route('/page1')
+def score_page1():
+    df = pd.read_csv('score.csv', index_col='지원번호')
+    df2 = df[['이름', '학교', '키', 'SW특기']]
+    df3 = df[['이름', '국어', '영어', '수학']]
+    return render_template('page1.html',
+                            table2=df2.to_html(classes='table table-striped', table_id='tbl2'),
+                            table3=df3.to_html(classes='table table-striped', table_id='tbl3'))
+
+@score.route('/graph')
+def score_graph():
+    return render_template('graph.html')
+
+@score.route('/chart/<subject>')
+def score_chart(subject):
+    plt.figure(figsize=(10,5), dpi=50)
+    df = pd.read_csv('score.csv', index_col='지원번호')
+    x=df['이름'].values
+    y=df[subject].values
+    plt.bar(x, y, color='#C6C491')
+    # plt.show()
+    img = BytesIO()
+    plt.savefig(img, format='png', dpi=100)
+    img.seek(0)
+    return send_file(img, mimetype='image/png')
